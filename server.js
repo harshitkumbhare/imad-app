94c4037a3091b337ea8de8ff13f7b8d39cc8ce87
harshitkumbhare <30654461+harshitkumbhare@users.noreply.github.com>
2017-08-04 13:23:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var atricles = {
+var articles = {
     'article-one': {
     title: 'Article One | Harshit Kumbhare',
     heading: 'Article One',
