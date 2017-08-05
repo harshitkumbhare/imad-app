1615f0a17db1672aa5913c93c0b31d153fdfa7c7
harshitkumbhare <30654461+harshitkumbhare@users.noreply.github.com>
2017-08-05 11:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var element = document.getElementById('main-text'
 element.innerHTML = 'New Value'; 
 
 //move the image
-var img = document.getElementById('modi');
+var img = document.getElementById('madi');
 img.oneclick = function () {
     img.style.marginleft = '100px';
 };
