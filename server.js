ea1c337c44bf74a6745366516d20c64503fef601
harshitkumbhare <30654461+harshitkumbhare@users.noreply.github.com>
2017-08-15 16:21:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'harshitkumbhare',
@@ -88,6 +89,17 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input, salt){
+    //how do we create a hash ?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('hash/:input', function(req,res){
+   var hashedString = hash(req.params.input, 'this-is-some-ramdom-string'); 
+   res.send(hashedString);
 });
 
 var pool= new Pool(config);
